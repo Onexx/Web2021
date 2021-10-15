b9c3ab6975a98b642c226b782e0a06c60737d5e4
Onexx <ilya7538@mail.ru>
2021-10-15 18:56:13 +0300

    Fixed small bugs in HW 3.3

--- Web 3/src/main/java/ru/itmo/wp/servlet/MessageServlet.java
+++ Web 3/src/main/java/ru/itmo/wp/servlet/MessageServlet.java
@@ -2,14 +2,12 @@ package ru.itmo.wp.servlet;
 
 import com.google.gson.Gson;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class MessageServlet extends HttpServlet {
@@ -22,34 +20,44 @@ public class MessageServlet extends HttpServlet {
             this.user = user;
             this.text = text;
         }
+
+        public String getUser() {
+            return user;
+        }
+
+        public String getText() {
+            return text;
+        }
     }
 
-    List<Message> messages = new ArrayList<>();
+    private final List<Message> messages = new ArrayList<>();
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String requestUri = request.getRequestURI();
         HttpSession session = request.getSession();
-        System.out.println(new Date().getTime());
         switch (requestUri) {
             case "/message/auth": {
                 String user = request.getParameter("user");
                 if (user != null) {
-                    session.setAttribute("user", user);
-                } else {
-                    user = "NoName";
+                    if(!user.trim().isEmpty()){
+                        session.setAttribute("user", user);
+                    }else{
+                        user = null;
+                    }
                 }
                 writeToJson(user, response);
                 break;
             }
             case "/message/findAll":
-                writeToJson(messages, response);
-                System.out.println(messages);
+                if (session.getAttribute("user") != null) {
+                    writeToJson(messages, response);
+                }
                 break;
             case "/message/add": {
                 String user = (String) session.getAttribute("user");
                 String text = request.getParameter("text");
-                if (user != null && text != null && !text.isEmpty()) {
+                if (user != null && text != null && !text.trim().isEmpty()) {
                     messages.add(new Message(user, text));
                 } else {
                     response.sendError(HttpServletResponse.SC_BAD_REQUEST);
@@ -60,7 +68,7 @@ public class MessageServlet extends HttpServlet {
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST);
                 break;
         }
-
+        response.setCharacterEncoding("windows-1251");
         response.setContentType("application/json");
     }
 
